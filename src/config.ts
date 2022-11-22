9e454a425315ae02b4f79b018dcb467fd5d5654b
ccsimmi <ffsimmi@gmail.com>
2022-11-22 23:38:11 +0000

    styled error page

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE_ERROR = '404 error';
+export const SITE_DESCRIPTION_ERROR =
+  'The page you are looking for cannot be found';
